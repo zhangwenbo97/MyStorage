09f51bd9969120bd3305174858b5e4968266ac58
zhangxiaobo <zhangwenbo97@126.com>
2020-11-19 14:56:44 +0800

    增加Person1类

---
+++ gitTest1/src/com/huawei/Person1.java
@@ -0,0 +1,13 @@
+package com.huawei;
+
+/**
+ * @Auther: zhangwenbo
+ * @Data: 2020/11/19 14:55
+ * @Description: gitTest com.huawei
+ * @Version: JDK15
+ */
+public class Person1 {
+    public static void main(String[] args) {
+        System.out.println("Person1");
+    }
+}
