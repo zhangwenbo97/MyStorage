e402a63fb81fa6539b4222f4e5a7b83b6bb945fa
zhangxiaobo <zhangwenbo97@126.com>
2020-11-19 14:52:29 +0800

    第一次提交gitTest1中的Person

---
+++ gitTest1/src/com/huawei/Person.java
@@ -0,0 +1,14 @@
+package com.huawei;
+
+/**
+ * @Auther: zhangwenbo
+ * @Data: 2020/11/19 14:41
+ * @Description: gitTest com.huawei
+ * @Version: JDK15
+ */
+public class Person {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("2");
+    }
+}
