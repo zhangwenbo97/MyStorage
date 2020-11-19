228ad175f6849fb7725c0b7f21e0fd4b20dbe0ab
zhangxiaobo <zhangwenbo97@126.com>
2020-11-19 15:02:17 +0800

    Person类中增加输出int 3

--- gitTest1/src/com/huawei/Person.java
+++ gitTest1/src/com/huawei/Person.java
@@ -5,10 +5,12 @@ package com.huawei;
  * @Data: 2020/11/19 14:41
  * @Description: gitTest com.huawei
  * @Version: JDK15
+ *
  */
 public class Person {
     public static void main(String[] args) {
         System.out.println("1");
         System.out.println("2");
+        System.out.println("3");
     }
 }
